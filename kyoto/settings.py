7bb5fc7a5ca7c6c27931e7a2a75d13417963e58b
Natalie Solovey <jimmoffeef@gmail.com>
2024-11-14 03:38:17 +0300

    обновление некоторых настроек

--- kyoto/settings.py
+++ kyoto/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "debug_toolbar",
     'main.apps.MainConfig'
 ]
 
@@ -58,7 +57,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'kyoto.urls'
