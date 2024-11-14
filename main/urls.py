147cc25b8637f2d4a4a6ef7b565f5e1bb8438f0e
Natalie Solovey <jimmoffeef@gmail.com>
2024-11-14 03:32:52 +0300

    исправление отображения некоторых фото

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path, reverse_lazy
 from django.contrib.auth.views import (
     PasswordChangeView, PasswordChangeDoneView,
-    PasswordResetView, PasswordResetDoneView,
+    PasswordResetView, PasswordResetDoneView, 
     PasswordResetConfirmView, PasswordResetCompleteView
 )
 from debug_toolbar.toolbar import debug_toolbar_urls
@@ -13,40 +13,40 @@ from .views import (
 )
 
 from django.views.decorators.cache import cache_page
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    # Главные страницы
+    # Main pages
     path('', cache_page(60)(views.index), name='home'),
     path('about-us', cache_page(60 * 15)(views.about), name='about'),
     path('chai', cache_page(60 * 15)(views.chai), name='chai'),
 
-    # Аутентификация
+    # Authentication
     path('register/', RegisterUser.as_view(), name='register'),
     path('login/', views.login, name='login'),
     path('authentication/', LoginUser.as_view(), name='authentication'),
     path('logout/', logout_user, name='logout'),
     path('profile/', ProfileUser.as_view(), name='profile'),
 
-    # Блог и посты
+    # Blog and posts
     path('blog/', views.blog, name='blog'),
     path('blog/<slug:post>/', views.categories, name='posts'),
     path('post/<slug:post_slug>/', views.show_post, name='post'),
     path('post/<int:post_id>/', views.show_post, name='post'),
     path('post/<int:pk>/', views.post, name='post'),
-    
-    # Теги
+
+    # Tags
     path('tag/<slug:tag_slug>/', views.tag_posts, name='tag_posts'),
 
-    # Комментарии
+    # Comments
     path('comment/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
     path('comment/<int:comment_id>/like/', views.toggle_comment_like, name='toggle_comment_like'),
     path('comment/<int:comment_id>/edit/', views.edit_comment, name='edit_comment'),
     path('comment/<int:comment_id>/reply/', views.reply_to_comment, name='reply_to_comment'),
 
-
-
-    # Управление паролем
-    path('password-change/', 
+    # Password management
+    path('password-change/',
         PasswordChangeView.as_view(template_name='main/password_change_form.html'),
         name='password_change'),
     path('password-change/done/',
@@ -64,4 +64,7 @@ urlpatterns = [
     path('password-reset/complete/',
         PasswordResetCompleteView.as_view(template_name="main/password_reset_complete.html"),
         name='password_reset_complete'),
-] + debug_toolbar_urls()
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
